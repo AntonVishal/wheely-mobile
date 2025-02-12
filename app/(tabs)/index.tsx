7595d2cad779be1c15d5baced981d6c5314cae99
antonvishal <antonvishal@appexert.com>
2025-02-12 10:39:34 +0530

    fix: formatting

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,26 @@
-import {ActivityIndicator, StyleSheet} from 'react-native';
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
-import {useEffect, useState} from "react";
-import {useExpoRouter} from "expo-router/build/global-state/router-store";
+import { ActivityIndicator, StyleSheet } from "react-native";
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View } from "@/components/Themed";
+import { useEffect, useState } from "react";
+import { useExpoRouter } from "expo-router/build/global-state/router-store";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function TabOneScreen() {
-  const router = useExpoRouter()
+  const router = useExpoRouter();
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-    const checkAuth = async()=>{
-      const token = await AsyncStorage.getItem('userToken');
-      if(!token){
-        router.replace('/onboarding')
-      }else{
+    const checkAuth = async () => {
+      const token = await AsyncStorage.getItem("userToken");
+      if (!token) {
+        router.replace("/onboarding");
+      } else {
         setLoading(false);
       }
-    }
+    };
     setTimeout(checkAuth, 2000);
   }, []);
-  if(loading){
-    return <ActivityIndicator size={"large"} style={{flex:1}} />
+  if (loading) {
+    return <ActivityIndicator size={"large"} style={{ flex: 1 }} />;
   }
   return (
     <View style={styles.container}>
@@ -32,16 +32,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
